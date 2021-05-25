ef28fe514ddbb35d468c283acf61f7477faaa952
Deiver Kiel Retana <deikiel@gmail.com>
2021-05-24 18:57:20 -0600

    Update random-file-generator.sh

--- random-file-generator.sh
+++ random-file-generator.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+# Usage
+# script.sh /path/to/create/files
+
+path=$1
+
 for i in {1..2048}
 do
     name=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13)
-    touch ${name}.testing
-    echo ${i} >> ${name}.testing
-    echo "${name}" >> ${name}.testing
+    new_file="${path}/${name}.testing"
+    touch ${new_file}
+    echo ${i} >> ${new_file}
+    echo "${name}" >> ${new_file}
 done
