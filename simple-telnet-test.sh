c6f89fc37eefd1526dd9c2a5ac3bc1a1377d6359
Deiver Kiel Retana <deikiel@gmail.com>
2021-07-27 13:18:53 -0600

    Update simple-telnet-test.sh

--- simple-telnet-test.sh
+++ simple-telnet-test.sh
@@ -5,7 +5,7 @@ port=389
 
 while read ip
 do
-    case $(telnet ${ip} ${port} </dev/null 2>&1 | tail -1) in
+    case $(timeout 5 telnet ${ip} ${port} </dev/null 2>&1 | tail -1) in
         (*closed*) echo "telnet to ${ip} connected " >>Telnet_results.txt
           ;;
         (*refused*) echo "telnet to ${ip} Refused " >>Telnet_results.txt
